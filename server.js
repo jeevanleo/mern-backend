670f964abf5a9112b7822a126ed46515b86ce7c1
Jeevan Babu Satyam <116184803+jeevanleo@users.noreply.github.com>
2024-06-05 23:08:10 +0530

    cors u

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
-const cors = require("cors"); // Import the cors middleware
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:3000", // Allow requests from this origin
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
-app.use(cors()); // Apply CORS middleware to the app
+// Apply CORS middleware to the app
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests from this origin
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 io.on("connection", (socket) => {
   console.log("User Online");
 
   socket.on("canvas-data", (data) => {
     socket.broadcast.emit("canvas-data", data);
+  });
+
+  socket.on("clear-canvas", () => {
+    socket.broadcast.emit("clear-canvas");
   });
 });
 
